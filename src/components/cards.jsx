05518ff3f016a692d1db888349ce98231cd8696f
sepehrbeyranvand <sepehr7797@gmail.com>
2024-04-12 22:47:16 +0330

    add details and time inputs and some css

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -3,45 +3,50 @@ import React, { useState } from "react";
 export default function Todo() {
   //Form variables and functions
   const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
   const [date, setDate] = useState("");
-  const [color, setColor] = useState("#c85b5b");
-  const colorio = (e) => {
-    setColor(e.target.value);
-  };
+  const [select, setSelect] = useState("");
+
   const resetAll = () => {
     setName("");
     setDate("");
-    setColor("");
+    setSelect("");
   };
   const submitForm = (e) => {
     console.log(date);
     e.preventDefault();
   };
-  const transferInfo=()=>{
-    const UsersObj={
-      name:name,
-      date:date,
-      color:color,
-      id:Math.floor(Math.random()*10000)
-    }
-    setUsers((prevUser)=>{
-      return [...prevUser,UsersObj]
-    })
-    resetAll()
-  }
-  const dlCard=(id)=>{
-    setUsers(users.filter((e)=>{
-      return id !==e.id
-    }))
-  }
+  const transferInfo = () => {
+    const UsersObj = {
+      name: name,
+      date: date,
+      select: select,
+      details: details,
+      id: Math.floor(Math.random() * 10000),
+    };
+    setUsers((prevUser) => {
+      return [...prevUser, UsersObj];
+    });
+    resetAll();
+  };
+  const dlCard = (id) => {
+    setUsers(
+      users.filter((e) => {
+        return id !== e.id;
+      })
+    );
+  };
   //Form variables and functions END here
 
   //Cards start here
-  const [users,setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+  const Done =()=>{
+    alert('Great !')
+  }
 
   return (
     <div className="row">
-      <form onSubmit={submitForm} className="d-flex flex-column gap-1 col-4">
+      <form onSubmit={submitForm} className="d-flex flex-column gap-1 col-4 mx-2">
         <label className="label-control" htmlFor="Name">
           Name :
         </label>
@@ -53,42 +58,76 @@ export default function Todo() {
           type="text"
         />
         <label className="label-control" htmlFor="Date">
-          {" "}
-          Date :
+          Until :
         </label>
         <input
           onChange={(e) => setDate(e.target.value)}
           value={date}
           className="form-control"
           id="Date"
-          type="date"
+          type="datetime-local"
         />
-        <label className="label-control" htmlFor="Color">
-          Color :
+        <label className="label-control" htmlFor="Select">
+          Select :
         </label>
-        <input
-          onChange={colorio}
-          value={color}
-          className="form-control w-25"
-          id="Color"
-          type="color"
-        />
-        <p style={{cursor:"pointer"}} onClick={resetAll} className="text-primary text-center">
+        <select
+          className="form-select"
+          onChange={(e) => setSelect(e.target.value)}
+          value={select}
+          name=""
+          id="Select"
+        >
+          <option value="Important">Important</option>
+          <option value="Mundane">Mundane</option>
+          <option value="Not important">Not important</option>
+        </select>
+        <label htmlFor="Details">Details :</label>
+        <textarea
+          onChange={(e) => setDetails(e.target.value)}
+          className="form-control"
+          value={details}
+          name=""
+          id="Details"
+          cols="10"
+          rows="5"
+        ></textarea>
+        <p
+          style={{ cursor: "pointer" }}
+          onClick={resetAll}
+          className="text-primary text-center"
+        >
           Reset All
         </p>
-        <button onClick={transferInfo} className="btn btn-success rounded-3 ">Submit</button>
+        <button onClick={transferInfo} className="btn btn-success rounded-3 ">
+          Submit
+        </button>
       </form>
-      <div className="col-7">
-    {
-      users.map((e,index)=>(
-        <div style={{background:e.color}} className="d-flex flex-column justify-content-center align-items-center p-2 border border-1 border-warning rounded-3 m-2">
-          <h1 className="text-primary">Name :{e.name === '' ? 'Empty' : e.name}</h1>
-          <p className="text-muted">Date :{e.date === ' ' ? 'Empty' : e.date}</p>
-          <p className="text-muted">ID :{e.id}</p>
-          <button onClick={()=> dlCard(e.id)} className="btn btn-danger rounded-3">Delete</button>
-        </div>
-      ))
-    }
+
+
+      {/* Cards */}
+
+      <div className="col-5">
+        {users.map((e, index) => (
+          <div className="card p-1">
+            <div className="card-header d-flex justify-content-between align-items-center">
+              <h1 className="card-title">{e.name}</h1>
+              <h5 className="text-success">DeadLine : {e.date}</h5>
+            </div>
+            <div className="card-body">
+              <p className="text-muted">{e.details}</p>
+              <p className="text-info">{e.select}</p>
+            </div>
+            <div className="card-footer d-flex justify-content-between align-items-center">
+              <button
+                onClick={() => dlCard(e.id)}
+                className="btn btn-danger rounded-3"
+              >
+                Delete
+              </button>
+              <button onClick={Done} className="btn btn-success rounded-3">Done</button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
